62b423a3d353859e67f462c0f80aab5a9e6ae901
James <oluwajamie@gmail.com>
2024-05-14 09:33:30 +0100

    week commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component'
 @Component({
   selector: 'app-root',
   standalone: true,
